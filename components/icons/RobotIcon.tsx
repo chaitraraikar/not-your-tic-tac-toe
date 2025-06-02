4e81866a53923cf59c1d6c8f874cc3b80b4cc2cd
chaitraraikar <108943758+chaitraraikar@users.noreply.github.com>
2025-06-02 21:06:46 +0530

    Update RobotIcon.tsx

--- components/icons/RobotIcon.tsx
+++ components/icons/RobotIcon.tsx
@@ -9,7 +9,7 @@ const RobotIcon: React.FC<{ className?: string }> = ({ className }) => (
     width="24" 
     height="24"
   >
-    {/* Corrected path data: "v робот8a2" changed to "v8a2" */}
+    {/* Path data for the robot icon */}
     <path d="M12 2a2 2 0 00-2 2v2H8a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2V8a2 2 0 00-2-2h-2V4a2 2 0 00-2-2zm0 4h.01M12 8a4 4 0 110 8 4 4 0 010-8zm-6 8a2 2 0 100 4 2 2 0 000-4zm12 0a2 2 0 100 4 2 2 0 000-4zM8 5h8v1H8V5z" />
     <path d="M7 10.5A1.5 1.5 0 018.5 9h7a1.5 1.5 0 010 3h-7A1.5 1.5 0 017 10.5zM9 14a1 1 0 011-1h4a1 1 0 110 2H10a1 1 0 01-1-1z" />
   </svg>
